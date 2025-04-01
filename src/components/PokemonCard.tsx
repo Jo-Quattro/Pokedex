24d7cae1a7152164592d8b6d0217a184c2362ed0
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-04-01 16:10:32 +0200

    added prop types

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,11 @@
-function PokemonCard({ pokemon }) {
+interface PokemonProps {
+  pokemon: {
+    name: string;
+    imgSrc?: string;
+  };
+}
+
+function PokemonCard({ pokemon }: PokemonProps) {
   return pokemon.imgSrc ? (
     <figure>
       <img src={pokemon.imgSrc} alt={pokemon.name} />

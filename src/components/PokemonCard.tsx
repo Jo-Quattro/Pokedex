714ce3e88cf0a806229c15bbdcbe8aef0dd15206
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-04-01 14:31:12 +0200

    added pokemonList and a condition if the pokemon doesn't have a picture

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,25 @@
+const pokemonList = [
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
-  return (
+  const pokemon = pokemonList[1];
+  return pokemon.imgSrc ? (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
-        alt="Charmander"
-      />
-      <figcaption>Charmander</figcaption>
+      <img src={pokemon.imgSrc} alt={pokemon.name} />
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  ) : (
+    <figure>
+      <p>???</p>
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }

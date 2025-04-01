3d9d4827a727266abcf244894c162e4087fcd469
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-04-01 12:58:55 +0200

    added PokemonCard with Charmander instead of Bulbasaur because that's my favorite LOL

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,13 @@
+function PokemonCard() {
+  return (
+    <figure>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+        alt="Charmander"
+      />
+      ,<figcaption>Chamander</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;

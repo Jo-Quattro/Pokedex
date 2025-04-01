40e1f35c3164f7bbeb3b33de8e856d73340617c2
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-04-01 20:07:40 +0200

    added pokemon selection buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 const pokemonList = [
   {
@@ -8,15 +9,31 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
   },
   {
-    name: "mew",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+    name: "Mew",
   },
 ];
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("Charmander");
+
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
+
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name");
+  }
+
   return (
-    <>
-      <PokemonCard pokemon={pokemonList[0]} />
-    </>
+    <div>
+      <PokemonCard pokemon={pokemon} />
+      <button type="button" onClick={() => setPokemonName("Charmander")}>
+        Charmander
+      </button>
+      <button type="button" onClick={() => setPokemonName("Mew")}>
+        Mew
+      </button>
+    </div>
   );
 }
 
